5b22ce6d4d53923d4faedeb64c59431c5e66098d
Douglas Mak <github@douglasmak.com>
2013-10-17 11:44:53 -0700

    moved factory into numbers class

--- commands.js
+++ commands.js
@@ -5,26 +5,6 @@ var MergeSort = Strategies.MergeSort;
 var InsertionSort = Strategies.InsertionSort;
 var QuickSortRandomPivot = Strategies.QuickSortRandomPivot;
 
-
-/***************************************************
-    Factory Pattern
-    Will return different orders of numbers depending on the caller
-****************************************************/
-
-
-var numbers = function(numberOfNumbers, type) {
-    var numbers = new Numbers(numberOfNumbers);
-    switch(type) {
-        case "reverse":
-            // return numbers.getArrayOfReverseNumbers();
-            break;
-        case "random":
-            return numbers.getArrayOfRandomNumbers();
-            break;
-    }
-}
-
-
 /*
     Command Pattern: Command Interface
     Used to enforce the implementation of execute. Also, used for tagging objects; The invoker 
@@ -40,28 +20,26 @@ CommandInterface.prototype.execute = function() {
     Used to enforce the implementation of execute. Also, used for tagging objects; The invoker 
     will only run commands that are an instance of this interface.
 */
-var SortCommand = function() {};
-SortCommand.prototype = new CommandInterface();
 
 var MergeSortCommand = function() {};
-MergeSortCommand.prototype = new SortCommand();
+MergeSortCommand.prototype = new CommandInterface();
 MergeSortCommand.prototype.execute = function() {
     var strategyContext = new StrategyContext(new MergeSort());
-    strategyContext.executeStrategy(numbers(10000000, "random"));
+    strategyContext.executeStrategy(new Numbers(10000000, "random"));
 };
 
 var InsertionSortCommand = function() {};
-InsertionSortCommand.prototype = new SortCommand();
+InsertionSortCommand.prototype = new CommandInterface();
 InsertionSortCommand.prototype.execute = function() {
     var strategyContext = new StrategyContext(new InsertionSort());
-    strategyContext.executeStrategy(numbers(10000, "random"));
+    strategyContext.executeStrategy(new Numbers(10000, "random"));
 };
 
 var QuickSortRandomPivotCommand = function() {};
-QuickSortRandomPivotCommand.prototype = new SortCommand();
+QuickSortRandomPivotCommand.prototype = new CommandInterface();
 QuickSortRandomPivotCommand.prototype.execute = function() {
     var strategyContext = new StrategyContext(new QuickSortRandomPivot());
-    strategyContext.executeStrategy(numbers(10000000, "random"));
+    strategyContext.executeStrategy(new Numbers(10000000, "random"));
 };   
 
 exports.MergeSortCommand = MergeSortCommand;
--- numbers.js
+++ numbers.js
@@ -1,21 +1,32 @@
-exports.Numbers = function(numberOfNumbers) { 
+exports.Numbers = function(numberOfNumbers, type) { 
     // constructor
     var numbers = [];
 
     if(!numberOfNumbers) {
         numberOfNumbers = 100; // default to 100 numbers 
     }
+
     
-    this.getArrayOfRandomNumbers = function() {
+    this.getRandomInt = function(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    switch(type) {
+        case "reverse":
+            // return numbers.getArrayOfReverseNumbers();
+            break;
+        case "random":
+            return getArrayOfRandomNumbers.call(this);
+            break;
+    }
+
+    // Private
+    function getArrayOfRandomNumbers() {
         while(numberOfNumbers) {
             numbers.push(this.getRandomInt(0, numberOfNumbers));
             numberOfNumbers--;
         }
         return numbers;
-    }
-
-    this.getRandomInt = function(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
 }
